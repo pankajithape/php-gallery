b9c470d61bcec7c2e572807d3c14f6940c5c5c95
pankajithape <pankajithape95@gmail.com>
2021-12-10 20:22:16 +0530

    photo delete redirect code

--- admin/delete_photo.php
+++ admin/delete_photo.php
@@ -5,15 +5,15 @@
 // }
 
 if (empty($_GET['id'])) {
-  redirect("../photos.php");
+  redirect("photos.php");
 }
 
 $photo = Photo::find_by_id($_GET['id']);
 if ($photo) {
   $photo->delete_photo();
-  redirect("../photos.php");
+  redirect("photos.php");
 } else {
-  redirect("../photos.php");
+  redirect("photos.php");
 }
 
 ?>
